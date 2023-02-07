0bb556b74d8058be7fcb55068c304553186bdf62
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-02-07 17:42:02 +0000

    fixed errors of asynchronous

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,23 +1,19 @@
 class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-   @book=Book.find(params[:book_id])
-   @book_comment = BookComment.new
-
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    @book_comment = BookComment.new
-
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
 
-  def post_comment_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
 
