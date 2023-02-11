4458ac2dc65a49c9fb0f68754cbf632c272531c7
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-02-11 00:37:54 +0000

    added groups

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :groups, except: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
